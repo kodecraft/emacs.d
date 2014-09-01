ca75b0ff96ea736e369f8ed037ce40575b6cb809
Umun Siow <kodecraft@gmail.com>
2014-09-01 11:13:02 +0800

    changed order of package repo because windows choking on "not found" error.

--- init.el
+++ init.el
@@ -12,9 +12,10 @@
 (add-to-list 'load-path "~/.emacs.d/elpa")
 
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ))
 (package-initialize)
 
 (defvar my-packages '(better-defaults ;; https://github.com/technomancy/better-defaults
